82ce6424828bd1d12bb738745037853b022d27e8
honerd <yash.nigam3006@gmail.com>
2022-12-07 12:05:22 +0530

    register endpoint handled and post request works

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,14 +1,17 @@
 import express  from "express";
 import Cors from "cors"
 import mongoose from "mongoose";
+import registerModel from "./Model.js";
 
 //app config
 const app=express();
-const port=process.env.PORT||5000;
+const port=process.env.PORT||5000; 
 //middlewares
 app.use(express.json());
 app.use(Cors());
 //dbconfig
+const mongo_url="mongodb+srv://yash_3006:mummypapa0705@todo-peopledata.ozjewc1.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(mongo_url);
 
 //endpoints
 
@@ -16,6 +19,26 @@ app.get('/',(req,res)=>
 {
     res.status(200).send("hey the program works");
 })
+
+app.get('/register',(req,res)=>
+{
+    res.status(200).send("this end point works");
+})
+
+app.post('/register',(req,res)=>{
+    const user=req.body;
+
+    registerModel.create(user,(err,data)=>{
+        if(err)
+        {
+            res.status(501).send(err);
+        }
+        else
+        {
+            res.status(201).send(data);
+        }
+    })
+});
 
 //listeners
 app.listen(port,()=>{
--- todo-frontend/src/components/RegisterPage.js
+++ todo-frontend/src/components/RegisterPage.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import "../css/register.css"
 import { Form } from 'react-router-dom'
 import image from '../images/backgroundimage.jpg'
+import axios from 'axios'
 
-
+const baseUrl="http://localhost:5000/register";
 
 
 function RegisterPage() {
@@ -24,6 +25,28 @@ function RegisterPage() {
   setFormInput(nextFormState);
   
 }
+
+const enterToDatabase=()=>
+{
+  console.log("enter to database called");
+  axios.post(baseUrl,formInput).then(()=>{console.log("Yes this works")});
+}
+
+const check=()=>
+{
+  if(formInput.name===""||formInput.email===""||formInput.password===""||formInput.confirm_password==="")
+  {
+    alert("enter the details necessary")
+  }
+  if(formInput.password===formInput.confirm_password)
+  {
+    enterToDatabase();
+  }
+  else
+  {
+    alert("Wrong Password");
+  }
+}
  
   return (
     <>
@@ -35,11 +58,12 @@ function RegisterPage() {
       Register
     </div>
     <div className="registerform">
-      <form action="/home" method="post">
+      <form>
         <div className="form_part">Name <br/><input type="text" name="name" value={formInput.name} onChange={handleChange} /></div>
         <div className="form_part">Email<br/><input type="email" name="email" value={formInput.email} onChange={handleChange} /></div>
         <div className="form_part">Password<br/><input type="password" name="password" id="form_password" value={formInput.password} onChange={handleChange}/></div>
-        <div className="form_part">Confirm Password<br/><input type="password" name="password" id="form_cpassword" value={formInput.confirm_password} onChange={handleChange}/></div>
+        <div className="form_part">Confirm Password<br/><input type="password" name="confirm_password" id="form_password" value={formInput.confirm_password} onChange={handleChange}/></div>
+        <div className="submit_button"><button type="submit" onClick={check}>Submit</button></div>
       </form>
     </div>
     </>
